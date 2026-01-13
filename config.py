97bfeb7bee62304bc1fa819d655317f3abc63286
igroc2003-png <igroc2003@gmail.com>
2026-01-13 11:21:40 +0700

    config.py

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-CHANNEL_ID = "@HR_JOB_s"  # username канала
+
+CHANNEL_ID = "@HR_JOB_s"      # для проверки подписки
+CHANNEL_NUMERIC_ID = -1001652876751  # ID канала для ловли постов
