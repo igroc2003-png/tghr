bda83ee123fae2891ab6115c9f080197f6d6d89f
igroc2003-png <igroc2003@gmail.com>
2026-01-13 13:01:58 +0700

    config.py

--- config.py
+++ config.py
@@ -2,7 +2,8 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN") or "PASTE_BOT_TOKEN_HERE"
 
-# numeric ID канала (обязательно -100...)
+# numeric ID канала
 CHANNEL_NUMERIC_ID = -1001652876751
 
-ADMIN_ID = 5108587018  # твой user_id
+# ❗ твой Telegram user_id
+ADMIN_ID = 5108587018
