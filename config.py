61da60a9a577075e435179a53748a861bcd9d2b6
igroc2003-png <igroc2003@gmail.com>
2026-01-13 11:56:55 +0700

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,14 @@
 import os
 
+# 🔐 Токен бота
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-CHANNEL_ID = "@HR_JOB_s"      # для проверки подписки
-CHANNEL_NUMERIC_ID = -1001652876751  # ID канала для ловли постов
+# 📢 Канал
+CHANNEL_ID = "@HR_JOB_s"
+CHANNEL_NUMERIC_ID = -1001652876751  # numeric ID канала
+
+# 👑 Администратор
+ADMIN_ID = 5108587018  # твой Telegram user_id
+
+if not BOT_TOKEN:
+    raise ValueError("❌ BOT_TOKEN не найден в переменных окружения")
