4d202a631ad96ff54a2eebc850b9da50c9a13a39
igroc2003-png <igroc2003@gmail.com>
2026-01-13 20:08:40 +0700

    Add files via upload

--- db.py
+++ db.py
@@ -1,11 +1,28 @@
+
 import sqlite3
 
-conn = sqlite3.connect("bot.db")
-cur = conn.cursor()
+conn = sqlite3.connect("users.db")
+cursor = conn.cursor()
 
-cur.execute("""CREATE TABLE IF NOT EXISTS users (
-    user_id INTEGER PRIMARY KEY,
-    tags TEXT
-)""")
-
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id INTEGER,
+    tag TEXT,
+    UNIQUE(user_id, tag)
+)
+""")
 conn.commit()
+
+def add_user_tag(user_id: int, tag: str):
+    cursor.execute(
+        "INSERT OR IGNORE INTO users (user_id, tag) VALUES (?, ?)",
+        (user_id, tag)
+    )
+    conn.commit()
+
+def get_users_by_tag(tag: str):
+    cursor.execute(
+        "SELECT user_id FROM users WHERE tag = ?",
+        (tag,)
+    )
+    return [row[0] for row in cursor.fetchall()]
