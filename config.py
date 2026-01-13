f3a0078d2dd03618614a377685cb5fa21c280e24
igroc2003-png <igroc2003@gmail.com>
2026-01-13 12:56:30 +0700

    config.py

--- config.py
+++ config.py
@@ -4,3 +4,5 @@ BOT_TOKEN = os.getenv("BOT_TOKEN") or "PASTE_BOT_TOKEN_HERE"
 
 # numeric ID канала (обязательно -100...)
 CHANNEL_NUMERIC_ID = -1001652876751
+
+ADMIN_ID = 5108587018  # твой user_id
